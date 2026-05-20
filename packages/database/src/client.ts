2acd471120cb5c8b125a528eb2ddf4d9ee343130
aalan-big <aalanallvesgt@gmail.com>
2026-05-20 15:47:53 -0300

    fix: remove prisma adapter-pg (ESM/CJS conflict on Node 22)

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,25 +1,8 @@
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
-
-const connectionString = (process.env.DIRECT_URL ?? process.env.DATABASE_URL ?? '')
-  .replace(/[?&]pgbouncer=true/, '')
-
-const pool = new Pool({
-  connectionString,
-  max:                         5,
-  idleTimeoutMillis:           20000,
-  connectionTimeoutMillis:     15000,
-  keepAlive:                   true,
-  keepAliveInitialDelayMillis: 10000,
-  ssl: { rejectUnauthorized: false },
-})
-
-const adapter = new PrismaPg(pool)
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
